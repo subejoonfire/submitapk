b9c90adde6b592e1f8f8e2a515e85e71b3d48414
subejoonfire <harlanmuradi@gmail.com>
2025-04-11 05:00:46 +0800

    adding iduser on submissions table to integrate the table with users id

--- app/Models/SubmitModel.php
+++ app/Models/SubmitModel.php
@@ -8,13 +8,13 @@ class SubmitModel extends Model
 {
     protected $table = 'submissions';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['nama_pengaju', 'no_hp', 'file_pdf', 'file_rar', 'link_drive', 'status'];
-    
+    protected $allowedFields = ['iduser', 'nama_pengaju', 'no_hp', 'file_pdf', 'file_rar', 'link_drive', 'status'];
+
     public function getAllSubmissions()
     {
         return $this->findAll();
     }
-    
+
     public function getSubmissionById($id)
     {
         return $this->find($id);
