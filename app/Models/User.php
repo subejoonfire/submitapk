25ea13c8f58819b74d1d360e986d00cb16af7aa1
subejoonfire <harlanmuradi@gmail.com>
2025-04-14 08:17:27 +0800

    adding level to allowed fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['username', 'password'];
+    protected $allowedFields    = ['username', 'password', 'level'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
